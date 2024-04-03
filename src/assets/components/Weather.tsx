c0d16a42d15209026eec31f1b942e828b297571f
BigBabyofTel <augustus.tb@outlook.com>
2024-04-03 19:46:12 +0200

    FEAT: api call successful

--- src/assets/components/Weather.tsx
+++ src/assets/components/Weather.tsx
@@ -1,35 +1,50 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import { useEffect, useState } from "react";
+
+const apiKey = import.meta.env.VITE_API_KEY;
 
 export default function Weather() {
-  const APIkey = "982f45c73f249b6ad789f61b951da41c"
-  const cityName = "Cairo";
-
-  const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}`;
+  const [searchTerm, setSearchTerm] = useState("");
+  const [data, setData] = useState({})
 
   interface WeatherData {
-    temp: number,
-    feels_like: number,
-    temp_min: number, 
-    temp_max: number,
-    humidity: number,
-    main: string,
-    description: string,
-    name: string
-
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    humidity: number;
+    main: string;
+    description: string;
+    name: string;
   }
-
-  const getWeatherData:Promise<WeatherData> = async () => {
-    try {
-      const data = await axios.get(endpoint);
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  useEffect(() => {
+    const getWeatherData = async () => {
+      const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&units=metric&appid=${apiKey}`;
+      try {
+        const response: AxiosResponse<WeatherData> = await axios.get(endpoint);
+        if (response.status !== 200) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data: WeatherData = response.data;
+        setData(data);
+        return data;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    console.log(getWeatherData())
+  }, [searchTerm]);
 
   return (
-    <div>
-      {getWeatherData}
+    <div className="mx-auto">
+      <form>
+        <input
+          className="border-4"
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </form>
     </div>
   );
 }
